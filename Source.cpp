564a2cb1cac45858b8f36be2af41fc67d35c8ed0
Frisel <47296723+Frisel@users.noreply.github.com>
2020-10-27 21:44:53 +0300

    создал динамический массив размерностью вектора А и перемешал его элементы

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,43 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+#include <iomanip>
 
 using namespace std;
 int main() {
 	setlocale(LC_ALL, "rus");
 	char buff[50];
+	vector <int> a;
+	int r, num;
+	cout << "Введите последовательность чисел." << endl << " Для выхода нажмитн q" << endl;
+	while (cin >> num)
+		a.push_back(num);
+	cout << "Ваша последовательность" << endl;
+	for (auto x : a)
+		cout << x << " ";
+	cout << "Длинна массива" << size(a) << endl;
+	int k = size(a);
+	//////////////////////////////////////
+	srand(time(0)); // генерация случайных чисел
+	int  *ptrarray = new int[k]; // создание динамического массива
+	for (int count = 0; count < k; count++)
+		ptrarray[count] = count+1; //заполнение массива
+	for (int i = 0; i < k; i++)
+	{
+		srand(time(0));////////////////////////////////
+		for (int i = 0; i < k; ++i)//////////////////// Перестановка случайных элементов массива
+			swap(ptrarray[i], ptrarray [rand() % k]);//
+	}
+	cout << "array = ";
+	for (int count = 0; count < k; count++)
+		cout << setprecision(2) << ptrarray[count] << "    ";
+	delete[] ptrarray; // высвобождение памяти
+	cout << endl;
+	//////////////////////////////////////////
 	ofstream fin("decode.txt");
-	fin << "Работа сфайлом";
+	for (auto x : a)
+		fin << x;
 	fin.close();
 	ifstream fon("decode.txt");
 	if (!fon.is_open())
